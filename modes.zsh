42fc8f38b04a152a712700964a4a22720709b9b0
Matthias Kainer <mail@matthias-kainer.de>
2020-03-21 18:40:02 +0100

    Initial commit

---
+++ modes.zsh
@@ -0,0 +1,109 @@
+BASEDIR=$(dirname "$0")
+local __ZSHMODES=()
+local __ZSHMODES_CURRENT_MODE=""
+local loadConfig() {
+    emulate -L bash
+    source "$BASEDIR/config.sh"
+
+    for f in $(ls $BASEDIR/configs/*.config); do
+        eval $(parse_config $f )
+        __ZSHMODES+=($(basename "$f" | sed -e 's/\.config$//'))
+    done
+}
+loadConfig
+
+local expand() {
+  eval printf '%s' "\"\${$1?}\""
+}
+
+local config_ssh() {
+    ssh-add -D
+    local var="__ZSHMODES_${__ZSHMODES_CURRENT_MODE}_ssh_file"
+    var="${(P)var}"
+    if [ ! -z "$var" ]; then
+        ssh-add "$var"
+    fi
+}
+
+local config_git() {
+    if [ -z "$__ZSHMODES_CURRENT_MODE" ]; then
+        return
+    fi
+    local user="__ZSHMODES_${__ZSHMODES_CURRENT_MODE}_git_user_name"
+    user="${(P)user}"
+    local email="__ZSHMODES_${__ZSHMODES_CURRENT_MODE}_git_user_email"
+    email="${(P)email}"
+    git config --global user.name $user
+    git config --global user.email $email
+}
+
+local config_env() {
+    if [ -z "$__ZSHMODES_CURRENT_MODE" ]; then
+        return
+    fi
+    envs="__ZSHMODES_${__ZSHMODES_CURRENT_MODE}_env"
+    envsToSet=("${(@f)$(local | grep "${envs}_" | sed -e "s/${envs}_//")}")
+    old="__ZSHMODES_current_env"
+    oldEnvs="${(P)old}"
+    if [ ! -z "$oldEnvs" ]; then
+        oldEnvs=(${(@f)oldEnvs})
+        for oldEnv in "${oldEnvs[@]}"
+        do
+            unset "$oldEnv"
+        done
+    fi
+    newEnvs=()
+    if [ ! -z "$envsToSet" ]; then
+        for env in "${envsToSet[@]}"
+        do
+            keyVal=("${(@s/=/)env}") 
+            newEnvs+=($keyVal[1])
+            export "${keyVal[1]}"="${keyVal[2]}"
+        done
+    fi
+    export "$old"="${(F)newEnvs}"
+}
+
+mode() {
+    __ZSHMODES_CURRENT_MODE=$1
+
+    if [[ ! " ${__ZSHMODES[@]} " =~ " ${__ZSHMODES_CURRENT_MODE} " ]]; then
+        echo "mode not found"
+        return -1
+    fi
+    config_ssh
+    config_git
+    config_env
+}
+
+_mode() {
+    compadd $__ZSHMODES[@]
+}
+
+orig_git=$(whereis git)
+local do_git() {
+  config_git
+  eval "$orig_git $*"
+}
+
+prompt_currentMode() {
+    local icon="__ZSHMODES_${__ZSHMODES_CURRENT_MODE}_p10k_prompt_icon"
+    local text="__ZSHMODES_${__ZSHMODES_CURRENT_MODE}_p10k_prompt_text"
+    local foregroundColor="__ZSHMODES_${__ZSHMODES_CURRENT_MODE}_p10k_prompt_foregroundColor"
+    local backgroundColor="__ZSHMODES_${__ZSHMODES_CURRENT_MODE}_p10k_prompt_backgroundColor"
+    icon="${(P)icon}"
+    text="${(P)text}"
+    foregroundColor="${(P)foregroundColor}"
+    backgroundColor="${(P)backgroundColor}"
+    if [ ! -z "$icon" ]; then
+      icon="$icon"
+    else
+      icon=💢
+    fi
+    p10k segment -i $icon -t "$text" -f "$foregroundColor" -b "$backgroundColor"
+}
+
+alias git='do_git'
+config_git
+
+compdef _mode mode
---
+++ readme.md
@@ -0,0 +1,67 @@
+# ZSH Modes
+
+Isolated ssh keys, git settings and env-variables in a single zsh session.
+
+![Readme](readme/profile.gif)
+
+## Why
+
+I have to switch between multiple ssh/git/envs in multiple terminal-tabs. That's a quick and dirty solution for this usecase.
+
+## Install
+
+First, git clone this directory, i.e. via
+
+`git clone https://github.com/MatthiasKainer/zsh-lightweight-modes.git ~/.zsh/plugins/zsh-lightweight-modes`
+
+Then, add the plugin to your zsh as you would. If have no idea what I'm talking about then add the following line to your `.zshrc`:
+
+```sh
+source '/Users/YOURUSER/.zsh/plugins/zsh-lightweight-modes/modes.zsh'
+```
+
+And reload you zsh. Now you can type `mode` and tab to see all modes you have available.
+
+## Adding a mode
+
+Open the directory where you installed the plugin, and add a new file into the `configs/` directory, with the extension `config`
+
+The format is similar to yaml (however it is not - see below), and looks like this:
+
+```yml
+git:
+  user:
+    name: Example
+    email: mail@example.com
+env:
+  quick: example
+  anotherquick: "another-example"
+  yetanotherquick: "another example"
+ssh:
+  file: /Users/example/.ssh/id_rsa
+p10k:
+  prompt:
+    icon: "🏃"
+    text: quick
+    foregroundColor: white
+    backgroundColor: white
+```
+
+| config | value |
+| -- | -- |
+| **git.user.name** | Your committer-name in this mode |
+| **git.user.email** | The email for the committer |
+| **env.*** | A list of environment variables to set for this mode |
+| **ssh.file** | the location of the default ssh file to use in this mode |
+| **p10k.prompt.icon** | If you use p10k, set this to have an icon in your prompt to see which mode you're in |
+| **p10k.prompt.text** | If you use p10k, set this to have text in your prompt to see which mode you're in |
+| **p10k.prompt.foregroundColor** | If you use p10k, set this to specify the foreground color (i.e. text) |
+| **p10k.prompt.backgroundColor** | If you use p10k, set this to set the background color |
+
+## NOT YAML
+
+Note that this config supports only a subset of yaml at the moment. Fields have always to be nested, chars like `-` are not supported in the names, and probably plenty more. Pls do create a pr if you have a better way to parse yaml in a shell.
+
+## Config p10k
+
+Add `currentMode` to your prompt elements, i.e. in your `.p10k.zsh` file.
